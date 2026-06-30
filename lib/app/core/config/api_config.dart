9fceefd5fa08f1b4215785cec26bef4f56674d65
jonnypaulino <jonnyzico10@gmail.com>
2026-06-30 10:10:23 -0300

    fix: update API base URL and increment version to 1.0.3+11

--- lib/app/core/config/api_config.dart
+++ lib/app/core/config/api_config.dart
@@ -2,7 +2,7 @@ class ApiConfig {
   ApiConfig._();
 
   // Variavel global da API base.
-  // static const String baseUrl = 'https://br-thp-meuben.azurewebsites.net';
-  static const String baseUrl = 'http://localhost:3000';
+  static const String baseUrl = 'https://br-thp-meuben.azurewebsites.net';
+  // static const String baseUrl = 'http://localhost:3000';
 
 }
